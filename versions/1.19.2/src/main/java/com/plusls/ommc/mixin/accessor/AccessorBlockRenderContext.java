ea517351994efe3600a0fab887e5a966e68c243d
Hendrix-Shen <HendrixShen@hendrixshen.top>
2023-07-27 21:08:50 +0800

    Improve `worldEaterMineHelper` impl on Sodium 0.4.9+
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

---
+++ versions/1.19.2/src/main/java/com/plusls/ommc/mixin/accessor/AccessorBlockRenderContext.java
@@ -0,0 +1,8 @@
+package com.plusls.ommc.mixin.accessor;
+
+import org.spongepowered.asm.mixin.Mixin;
+import top.hendrixshen.magiclib.compat.preprocess.api.DummyClass;
+
+@Mixin(DummyClass.class)
+public interface AccessorBlockRenderContext {
+}
