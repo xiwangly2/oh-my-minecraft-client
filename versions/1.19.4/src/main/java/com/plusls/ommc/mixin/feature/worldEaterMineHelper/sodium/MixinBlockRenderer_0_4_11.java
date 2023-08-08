2cabb0da5031a92673e97efa6d6607ab7359dde8
Hendrix-Shen <HendrixShen@hendrixshen.top>
2023-08-08 22:43:42 +0800

    Add back Sodium Stable version support on MC 1.19.4 (#18)
    - WTF is this?
    
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

--- versions/1.19.4/src/main/java/com/plusls/ommc/mixin/feature/worldEaterMineHelper/sodium/MixinBlockRenderer_0_4_11.java
+++ versions/1.19.4/src/main/java/com/plusls/ommc/mixin/feature/worldEaterMineHelper/sodium/MixinBlockRenderer_0_4_11.java
@@ -2,28 +2,30 @@ package com.plusls.ommc.mixin.feature.worldEaterMineHelper.sodium;
 
 import com.plusls.ommc.feature.worldEaterMineHelper.WorldEaterMineHelperUtil;
 import com.plusls.ommc.mixin.accessor.AccessorBlockRenderContext;
-import me.jellysquid.mods.sodium.client.render.chunk.compile.buffers.ChunkModelBuilder;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildBuffers;
 import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderContext;
 import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderer;
+import me.jellysquid.mods.sodium.client.render.chunk.data.ChunkRenderBounds;
 import net.minecraft.client.resources.model.BakedModel;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Dynamic;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.hendrixshen.magiclib.dependency.api.annotation.Dependencies;
 import top.hendrixshen.magiclib.dependency.api.annotation.Dependency;
 
-//#if MC == 11904
-//$$ import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildBuffers;
-//$$ import me.jellysquid.mods.sodium.client.render.chunk.data.ChunkRenderBounds;
-//$$ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-//#endif
-
-@Dependencies(and = @Dependency(value = "sodium", versionPredicate = ">0.4.8 <0.4.11"))
+/**
+ * The implementation for mc [1.19.3, ~)
+ */
+@Dependencies(and = @Dependency(value = "sodium", versionPredicate = ">0.4.10 <0.5"))
 @Mixin(value = BlockRenderer.class, remap = false)
-public abstract class MixinBlockRenderer_0_4_9 {
+public abstract class MixinBlockRenderer_0_4_11 {
     @Shadow(remap = false)
-    public abstract boolean renderModel(BlockRenderContext ctx, ChunkModelBuilder buffers);
+    public abstract void renderModel(BlockRenderContext ctx, ChunkBuildBuffers buffers, ChunkRenderBounds.Builder bounds);
 
     @Unique
     private final ThreadLocal<Boolean> ommc$renderTag = ThreadLocal.withInitial(() -> false);
@@ -35,8 +37,7 @@ public abstract class MixinBlockRenderer_0_4_9 {
                     value = "RETURN"
             )
     )
-    private void postRenderModel(@NotNull BlockRenderContext ctx, ChunkModelBuilder buffers, CallbackInfoReturnable<Boolean> cir
-    ) {
+    private void postRenderModel(@NotNull BlockRenderContext ctx, ChunkBuildBuffers buffers, ChunkRenderBounds.Builder bounds, CallbackInfo ci) {
         if (WorldEaterMineHelperUtil.shouldUseCustomModel(ctx.state(), ctx.pos()) && !this.ommc$renderTag.get()) {
             BakedModel customModel = WorldEaterMineHelperUtil.customModels.get(ctx.state().getBlock());
 
@@ -47,7 +48,7 @@ public abstract class MixinBlockRenderer_0_4_9 {
                 BakedModel originalModel = ctx.model();
                 // ((AccessorBlockStateBase) ctx.state()).setLightEmission(15);
                 ((AccessorBlockRenderContext) ctx).setModel(customModel);
-                this.renderModel(ctx, buffers);
+                this.renderModel(ctx, buffers, bounds);
                 ((AccessorBlockRenderContext) ctx).setModel(originalModel);
                 // ((AccessorBlockStateBase) ctx.state()).setLightEmission(originalLightEmission);
                 this.ommc$renderTag.set(false);
